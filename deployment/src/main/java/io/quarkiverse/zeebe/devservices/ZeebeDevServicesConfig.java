778864e4efbdc871b477a5db6b6731f4b297bd47
Andrej Petras <andrej@lorislab.org>
2022-11-03 17:34:49 +0100

    chore(deps): update zeebe-dev-monitor to 0.5.0 (#111)
    * chore(deps): update zeebe-dev-monitor to 0.4.0
    
    * chore(deps): update zeebe-dev-monitor to 0.5.0

--- deployment/src/main/java/io/quarkiverse/zeebe/devservices/ZeebeDevServicesConfig.java
+++ deployment/src/main/java/io/quarkiverse/zeebe/devservices/ZeebeDevServicesConfig.java
@@ -141,7 +141,7 @@ public class ZeebeDevServicesConfig {
         /**
          * The container image name to use, for container based zeebe simple monitor.
          */
-        @ConfigItem(name = "image-name", defaultValue = "ghcr.io/lorislab/zeebe-dev-monitor:0.3.0")
+        @ConfigItem(name = "image-name", defaultValue = "ghcr.io/lorislab/zeebe-dev-monitor:0.5.0")
         public String imageName;
 
         /**
