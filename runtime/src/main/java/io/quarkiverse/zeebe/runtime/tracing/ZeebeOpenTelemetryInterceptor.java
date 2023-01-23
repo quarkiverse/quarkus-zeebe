067b784db88ed24d7856fc7c379b506c2624ea28
Andrej Petras <andrej@lorislab.org>
2023-01-23 09:32:18 +0100

    chore(deps): update maven dependencies (#126)

--- runtime/src/main/java/io/quarkiverse/zeebe/runtime/tracing/ZeebeOpenTelemetryInterceptor.java
+++ runtime/src/main/java/io/quarkiverse/zeebe/runtime/tracing/ZeebeOpenTelemetryInterceptor.java
@@ -1,6 +1,6 @@
 package io.quarkiverse.zeebe.runtime.tracing;
 
-import static io.quarkus.opentelemetry.runtime.OpenTelemetryConfig.INSTRUMENTATION_NAME;
+import static io.quarkus.opentelemetry.runtime.config.OpenTelemetryConfig.INSTRUMENTATION_NAME;
 import static java.lang.String.valueOf;
 
 import java.util.Map;
