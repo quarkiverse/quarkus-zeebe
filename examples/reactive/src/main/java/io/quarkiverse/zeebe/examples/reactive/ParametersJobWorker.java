f0d25bfc0fe678da3c20b66ce179d39c9b257603
Andrej Petras <andrej@lorislab.org>
2023-03-09 23:06:17 +0100

    chore(example): add reactive parameter example

---
+++ examples/reactive/src/main/java/io/quarkiverse/zeebe/examples/reactive/ParametersJobWorker.java
@@ -0,0 +1,33 @@
+package io.quarkiverse.zeebe.examples.reactive;
+
+import io.quarkiverse.zeebe.JobWorker;
+import io.quarkiverse.zeebe.Variable;
+import io.quarkiverse.zeebe.VariablesAsType;
+import io.quarkus.logging.Log;
+import io.smallrye.mutiny.Uni;
+
+import java.util.UUID;
+
+public class ParametersJobWorker {
+
+
+    @JobWorker(type = "create-param")
+    public Uni<Parameter> createParam(@Variable("_info") String info) {
+        Parameter p = new Parameter();
+        p.info = info;
+        p.data = UUID.randomUUID().toString();
+        return Uni.createFrom().item(p);
+    }
+
+    @JobWorker(type = "update-param")
+    public Uni<Parameter> updateParam(@VariablesAsType Parameter parameter) {
+        parameter.data = "Hi, " + parameter.info;
+        return Uni.createFrom().item(parameter);
+    }
+
+    @JobWorker(type = "info-param")
+    public Uni<Void> infoParam(@VariablesAsType Parameter parameter) {
+        Log.infof("%s", parameter);
+        return Uni.createFrom().voidItem();
+    }
+}
