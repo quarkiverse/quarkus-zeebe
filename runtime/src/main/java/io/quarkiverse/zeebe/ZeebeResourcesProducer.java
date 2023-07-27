7fb0f2386158c8f20c56de73b40aba46e734a12c
mklimenko <mklimenko@systematica-consulting.ru>
2023-07-27 23:25:35 +0300

    Added an object mapper injection

--- runtime/src/main/java/io/quarkiverse/zeebe/ZeebeResourcesProducer.java
+++ runtime/src/main/java/io/quarkiverse/zeebe/ZeebeResourcesProducer.java
@@ -2,7 +2,9 @@ package io.quarkiverse.zeebe;
 
 import javax.enterprise.inject.Produces;
 import javax.inject.Singleton;
+import javax.inject.Inject;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.camunda.zeebe.client.api.JsonMapper;
 import io.camunda.zeebe.client.impl.ZeebeObjectMapper;
 import io.quarkus.arc.DefaultBean;
@@ -12,12 +14,19 @@ import io.smallrye.mutiny.infrastructure.Infrastructure;
 @Singleton
 public class ZeebeResourcesProducer {
 
+    protected final ObjectMapper objectMapper;
+
+    @Inject
+    public ZeebeResourcesProducer(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
     @Produces
     @Singleton
     @Unremovable
     @DefaultBean
     public JsonMapper defaultJsonMapper() {
-        return new ZeebeObjectMapper();
+        return new ZeebeObjectMapper(objectMapper);
     }
 
     @Produces
